856e6d53c40dc8735f4c27f4f282bb2b0aed9a08
Student <student@Administrators-Mac-mini.local>
2025-10-16 11:47:29 +0300

    teooria tegemine

--- index.js
+++ index.js
@@ -55,3 +55,17 @@
 // console.log(x, typeof x);
 // console.log(y, typeof y);
 // console.log(z, typeof z);
+
+
+// Const = a variable that can't be changed
+
+const pi = 3.14159;
+let radius;
+let circumference;
+
+document.getElementById("mysubmit").onclick = function(){
+    radius = document.getElementById("mytext").value;
+    radius = Number(radius);
+    circumference = 2 * pi * radius;
+    document.getElementById("myh3").textContent = circumference + "cm";
+}
