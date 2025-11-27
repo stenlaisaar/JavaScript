02da56cf0c23c6c5d946a2632d23c9071f0de285
Student <student@Administrators-Mac-mini.local>
2025-11-27 15:26:13 +0200

    teooria

--- Score quest/script.js
+++ Score quest/script.js
@@ -1,40 +1,40 @@
 // Algandmed
-const scores = [45, 88, 32, 99, 74, 50, 61, 92, 39];
-let currentScores = [...scores]; // koopia, mida saab muuta
+// const scores = [45, 88, 32, 99, 74, 50, 61, 92, 39];
+// let currentScores = [...scores]; // koopia, mida saab muuta
  
 // -------------------------------------------------------------
 // Viited DOM-ile
 // -------------------------------------------------------------
-const scoreList = document.getElementById("scoreList");
-const result = document.getElementById("result");
+// const scoreList = document.getElementById("scoreList");
+// const result = document.getElementById("result");
  
-const kpiCount = document.getElementById("kpi-count");
-const kpiAvg = document.getElementById("kpi-avg");
-const kpiTh = document.getElementById("kpi-th");
+// const kpiCount = document.getElementById("kpi-count");
+// const kpiAvg = document.getElementById("kpi-avg");
+// const kpiTh = document.getElementById("kpi-th");
  
-const countInput = document.getElementById("count");
+// const countInput = document.getElementById("count");
 
 // Tulemuste kuvamine
 
-function renderScores(){
-    scoreList.innerHTML = "";
-    currentScores.forEach((score) => {
-        const item = document.createElement("p");
-        item.textContent = score;
-        scoreList.appendChild(item);
-    });
-}
+// function renderScores(){
+    // scoreList.innerHTML = "";
+    // currentScores.forEach((score) => {
+        // const item = document.createElement("p");
+        // item.textContent = score;
+        // scoreList.appendChild(item);
+    // });
+// }
 
 // +5 punkti kõigile
 
-function addBonus(){
-    currentScores = currentScores.map((score) => score + 5);
-    renderScores(currentScores);
-}
+// function addBonus(){
+    // currentScores = currentScores.map((score) => score + 5);
+    // renderScores(currentScores);
+// }
 
 // Filter. Jätab alles ainult need, mille väärtus on 60 või suurem
 
-function filterByThreshold(){
-    currentScores = currentScores.filter((score) <= 60);
-    renderScores(filterByThreshold);
-}
+// function filterByThreshold(){
+    // currentScores = currentScores.filter((score) <= 60);
+    // renderScores(filterByThreshold);
+// }
--- index.js
+++ index.js
@@ -615,3 +615,225 @@
 
 // person1.eat();
 // person2.eat();
+
+
+
+// destructturing = extract values from arrays and objects,
+//                  then assing them to variables in a convinient way
+//                  [] = to perform array destructuring
+//                  {} = to perform object destructuring
+//                   5 examples
+
+// ------- EXAMPLE 1 -------
+// SWAP THE VALUE OF TWO VARIABLES
+
+// let a = 1;
+// let b = 2;
+
+// [a, b] = [b, a];
+
+// console.log(a);
+// console.log(b);
+
+
+// ------- EXAMPLE 2 -------
+// ELEMETNS IN AN ARRAY
+
+// const colors = ["red", "green", "blue", "black", "white"];
+
+// [colors[0], colors[4]] = [colors[4], colors[0]];
+
+// console.log(colors);
+
+// ------- EXAMPLE 3 -------
+// ASSIGN ARRAY ELEMENTS TO VARIABLES
+
+// const colors = ["red", "green", "blue", "black", "white"];
+
+// const [firstColor, secoundColor, thirdColor, ...extraColors] = colors;
+
+// console.log(firstColor);
+// console.log(secoundColor);
+// console.log(thirdColor);
+// console.log(extraColors);
+
+// ------- EXAMPLE 4 -------
+// EXTRACT VALUES FROM OBJECTS
+
+// const person1 = {
+    // firstName: "Spongebob",
+    // lastName: "Squarepants",
+    // age: 30,
+    // job: "Fry Cook",
+// }
+
+// const person2 = {
+    // firstName: "Patrick",
+    // lastName: "Star",
+    // age: 34,
+// }
+
+// const {firstName, lastName, age, job="Unemployed"} = person2;
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+// console.log(job);
+
+// ------- EXAMPLE 5 -------
+// DESTRUCTURE IN FUNCTION PARAMETERS
+
+// function displayPerson({firstName, lastName, age, job="Unemployed"}){
+    // console.log(`name: ${firstName} ${lastName}`);
+    // console.log(`age: ${age}`);
+    // console.log(`job: ${job}`);
+// }
+
+// const person1 = {
+    // firstName: "Spongebob",
+    // lastName: "Squarepants",
+    // age: 30,
+    // job: "Fry Cook",
+// }
+
+// const person2 = {
+    // firstName: "Patrick",
+    // lastName: "Star",
+    // age: 34,
+// }
+
+// displayPerson(person1);
+
+
+
+
+
+
+// nested objects = Objects inside of other Objects.
+//                  Allows you to represent more complex data structures
+//                  Chill object is enclosed by a Parent object
+
+//                  Person{Address{}, ContactInfo{}}
+//                  ShoppingCart{Keyboard{}, Mouse{}, Monitor{}}
+
+// const person = {
+    // fullName: "Spongebob Squarepants",
+    // age: 30,
+    // isStudent: true;
+    // hobbies: ["karate", "jellyfishing", "cooking"],
+    // address: {
+        // street: "124 Conch St.",
+        // city: "Bikini Bottom",
+        // country: "Int. Water"
+    // }
+// }
+
+// console.log(person.fullName);
+// console.log(person.age);
+// console.log(person.isStudent);
+// console.log(person.hobbies[2]);
+// console.log(person.address.city);
+
+
+
+
+// const person = {
+    // fullName: "Spongebob Squarepants",
+    // age: 30,
+    // isStudent: true;
+    // hobbies: ["karate", "jellyfishing", "cooking"],
+    // address: {
+        // street: "124 Conch St.",
+        // city: "Bikini Bottom",
+        // country: "Int. Water"
+    // }
+// }
+
+// for(const property in person.address){
+    // console.log(person.address[property]);
+// }
+
+
+
+
+// class Person{
+
+    // constructor(name, age, ...address){
+        // this.name = name;
+        // this.age = age;
+        // this.address = new Address(...address);
+    // }
+// }
+
+// class Address{
+
+    // constructor(street, city, country){
+        // this.street = street;
+        // this.city = city;
+        // this.country = country;
+    // }
+// }
+
+// const person1 = new Person("Spongebob", 30, "124 Conch St.", "Bikini Bottom", "Int Waters");
+
+// const person1 = new Person("Patrcik", 37, "128 Conch St.", "Bikini Bottom", "Int Waters");
+
+// const person1 = new Person("Squidward", 45, "126 Conch St.", "Bikini Bottom", "Int Waters");
+
+// console.log(person1.address.city);
+
+
+
+
+
+// ARREY OF OBJECTS
+
+const fruits = [{name: "apple", color: "red", caloreies: 95},
+                {name: "orange", color: "orange", caloreies: 45},
+                {name: "banana", color: "yellow", caloreies: 105},
+                {name: "coconut", color: "white", caloreies: 159},
+                {name: "pineapple", color: "yellow", caloreies: 37}];
+
+
+// fruits.pop();
+// fruits.splice(2, 4);
+
+// fruits.push({name: "grapes", color: "purple", caloreies: 62});
+
+// console.log(fruits);
+
+//------------- forEach () ----------
+
+// fruits.forEach(fruit => console.log(fruit.name));
+
+// ------------- map() ------------
+
+// const fruitNames = fruits.map(fruit => fruit.name);
+// const fruitColors = fruits.map(fruit => fruit.color);
+// const fruitCalories = fruits.map(fruit => fruit.calories);
+
+// console.log(fruitNames);
+// console.log(fruitColors);
+// console.log(fruitCalories);
+
+// ------------- filter() -------------
+
+// const yellowFruits = fruits.filter(fruit => fruit.color === "yellow");
+// const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+// const highCalFruits = fruits.filter(fruit => fruit.caloreies >= 100);
+
+// console.log(highCalFruits);
+
+
+// ------------ reduce() -------------
+
+// const maxFruit = fruits.reduce( (max. fruit) =>
+                                // fruit.calories > max.calories ?
+                                // fruit : max);
+
+// const minFruit = fruits.reduce( (min, fruit) =>
+                                // fruit.calories < min.caloreies ?
+                                // fruit : min);
+
+// console.log(maxFruit);
+// console.log(minFruit);
